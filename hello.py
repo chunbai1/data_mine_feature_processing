eda3a3a9d97ce0da633d13b4d961b433f089936a
白开水mq <1690831472@qq.com>
2023-06-02 16:42:54 +0800

    why the result of pull is different???QAQ

--- hello.py
+++ hello.py
@@ -5,5 +5,10 @@ level = input("Please input the game level(easy or midiem or hard):")
 
 print("Ok! Let's begin!")
 print("to be continued...")
-
 print("ok! Let't going on!")
+print("********************************")
+direction = input("Now the tree in on your left, the car is on your right, the dog is infront of you, the wall is behind of you. Now is your turn to select:")
+if direction == "behind":
+    print("You Win!")
+else:
+    print("You Died!")
